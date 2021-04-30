3def706d6f9352425fab22168d9403a484ec9e4f
SAIF <zarraisaifedine@gmail.com>
2021-04-30 02:56:46 +0200

    mmm

--- index.js
+++ index.js
@@ -13,19 +13,20 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
-import path from 'path';
 
-//if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(path.dirname(import.meta.url) + '../client/build')))
-  app.get('/*', (req, res) => {
-      res.sendFile(path.join(path.dirname(import.meta.url) +'../client/build', 'index.html'))
-  })
-//}
+import * as path from "path";
+const __dirname = path.resolve(path.dirname(''));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')))
+    app.get('/*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'))
+    })
+}
 const CONNECTION_URL = 'mongodb+srv://firasdb:firas123456789@cluster0.3hplv.mongodb.net/firasdb?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-  .catch((error) => console.log(`${err} did not connect`));
+    .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+    .catch((error) => console.log(`${err} did not connect`));
 
 mongoose.set('useFindAndModify', false);
