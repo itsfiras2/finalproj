ecde66740c786f98bf8d4beba96853b22019cd27
Firas <77270818+itsfiras2@users.noreply.github.com>
2022-04-17 01:08:35 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = 'mongodb+srv://firasdb:firas123456789@cluster0.3hplv.mongodb.net/firasdb?retryWrites=true&w=majority';
+const CONNECTION_URL = 'mongodb+srv://firasdb:firas123@cluster0.3hplv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
